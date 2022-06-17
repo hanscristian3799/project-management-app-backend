c8cece140b14ebec7543f2ce3c4eba1a5c23c781
hanscristian3799 <hanscristian376@gmail.com>
2022-06-17 20:26:21 +0700

    add cors and add cascade delete project on client delete

--- server/index.js
+++ server/index.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const colors = require("colors");
+const cors = require("cors");
 require("dotenv").config();
 const port = process.env.PORT || 5000;
 const { graphqlHTTP } = require("express-graphql");
 const app = express();
 const schema = require("./graphql/schema/schema");
 const connectDB = require("./config/db");
+
+app.use(cors("*"));
 
 connectDB();
 
